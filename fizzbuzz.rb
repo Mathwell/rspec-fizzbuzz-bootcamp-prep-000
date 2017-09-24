7849156e586ec22cf572eeb68960a93d17958ef0
Olena Ageyeva <eageeva@hotmail.com>
2017-09-24 14:41:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 def fizzbuzz(number=0)
  divby3=number/3
- diveby5=number/5
- puts divby3
- puts divby3.floor
- puts divby3==divby3.floor
+ divby5=number/5
+ puts divby5
+ puts divby5.floor
+ puts divby5==divby5.floor
  if (divby3==divby3.floor && divby5==divby5.floor)
    return "FizzBuzz"
  elsif (divby3==divby3.floor)
     return "Fizz"
- elsif (diveby5==diveby5.floor)
+ elsif (divby5==divby5.floor)
    return "Buzz"
  else return ""
  end
